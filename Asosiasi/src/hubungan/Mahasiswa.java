294177cf7de5c6815266598333c9e83ec27b45cf
Kholil Irsyad Marwan <marwansi4524110@univpancasila.ac.id>
2025-10-29 16:52:38 +0700

    Add files via upload

---
+++ Asosiasi/src/hubungan/Mahasiswa.java
@@ -0,0 +1,16 @@
+package hubungan;
+
+public class Mahasiswa {
+    private String NPM;
+    private String namaMahasiswa;
+
+    public Mahasiswa(String NPM, String namaMahasiswa) {
+        this.NPM = NPM;
+        this.namaMahasiswa = namaMahasiswa;
+    }
+
+    public void getInfoMahasiswa() {
+        System.out.println("NPM " + this.NPM);
+        System.out.println("Nama " + this.namaMahasiswa);
+    }
+}
